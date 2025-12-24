93afb2088564cc6935836cd3396fd8ebe3b323b3
Karim <karim2468mahmoud2468@gmail.com>
2025-12-24 18:43:12 +0200

    Add user management features

---
+++ SurveyBasket.API/Contracts/Users/UpdateUserRequest.cs
@@ -0,0 +1,9 @@
+namespace SurveyBasket.Contracts.Users;
+
+public record UpdateUserRequest
+(
+    string FirstName,
+    string LastName,
+    string Email,
+    IList<string> Roles
+);
--- SurveyBasket.API/Errors/UserErrors.cs
+++ SurveyBasket.API/Errors/UserErrors.cs
@@ -5,6 +5,12 @@ public static class UserErrors
     public static readonly Error InvalidCredentials =
         new("User.InvalidCredentials", "Invalid email/password", StatusCodes.Status401Unauthorized);
 
+    public static readonly Error DisabledUser =
+        new("User.DisabledUser", "Disabled user, please contact your administrator", StatusCodes.Status401Unauthorized);
+
+    public static readonly Error LockedUser =
+        new("User.LockedUser", "Locked user, please contact your administrator", StatusCodes.Status401Unauthorized);
+
     public static readonly Error InvalidJwtToken =
         new("User.InvalidJwtToken", "Invalid Jwt token", StatusCodes.Status401Unauthorized);
 
@@ -22,4 +28,10 @@ public static class UserErrors
 
     public static readonly Error DuplicatedConfirmation =
         new("User.DuplicatedConfirmation", "Email already confirmed", StatusCodes.Status400BadRequest);
+
+    public static readonly Error UserNotFound =
+    new("User.UserNotFound", "User is not found", StatusCodes.Status404NotFound);
+
+    public static readonly Error InvalidRoles =
+        new("Role.InvalidRoles", "Invalid roles", StatusCodes.Status400BadRequest);
 }
